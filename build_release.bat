4740ade3d8f4c941ed8431b32e2d2d5038f14387
Tom Barrett <thomasjbarrett@live.com>
2022-11-19 05:45:20 -0600

    fixed typo in css xcopy

--- build_release.bat
+++ build_release.bat
@@ -8,12 +8,12 @@ if not exist "%root%\release\MorningReader\css" mkdir "%root%\release\MorningRea
 if not exist "%root%\release\MorningReader\images" mkdir "%root%\release\MorningReader\images"
 if not exist "%root%\release\MorningReader\scripts" mkdir "%root%\release\MorningReader\scripts"
 
-xcopy "%root%manifest.json" "%root%\release\MorningReader" /Y
-xcopy "%root%options.html" "%root%\release\MorningReader" /Y
-xcopy "%root%README.md" "%root%\release\MorningReader" /Y
+xcopy "%root%manifest.json" "%root%release\MorningReader" /Y
+xcopy "%root%options.html" "%root%release\MorningReader" /Y
+xcopy "%root%README.md" "%root%release\MorningReader" /Y
 
-xcopy "%root%css %root%\release\MorningReader\css" /E /Y
-xcopy "%root%images" "%root%\release\MorningReader\images" /E /Y
-xcopy "%root%scripts" "%root%\release\MorningReader\scripts" /E /Y
+xcopy "%root%css" "%root%release\MorningReader\css" /E /Y
+xcopy "%root%images" "%root%release\MorningReader\images" /E /Y
+xcopy "%root%scripts" "%root%release\MorningReader\scripts" /E /Y
 
 "C:\Program Files\7-Zip\7z.exe" a "%root%\release\MorningReader.zip" "%root%\release\MorningReader"
